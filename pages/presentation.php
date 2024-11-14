467aabec3d87625ab212684de6bd465beee4aa99
Nawaank <mirtindayot@gmail.com>
2024-11-14 16:24:05 +0100

    chemin photo presentation

--- pages/presentation.php
+++ pages/presentation.php
@@ -3,13 +3,13 @@
 <div class="content">
     <h2>Présentation</h2>
     <div class="presentation">
-        <img src="assets/images/photo1.jpg" alt="Martin DAYOT" class="photo-profil">
+        <img src="../assets/images/photo_cv.jpg" alt="Martin DAYOT" class="photo-profil">
         <div class="description">
             <p>Bonjour, je suis Martin DAYOT. Passionné de photographie, je capture des moments uniques. Voici quelques-unes de mes œuvres.</p>
         </div>
-        <img src="assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
-        <img src="assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
-        <img src="assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
+        <img src="../assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
+        <img src="../assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
+        <img src="../assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
     </div>
 </div>
 
