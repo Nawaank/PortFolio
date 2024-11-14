dfd168552159db82003fac3d94b0b76bd8bfb5ea
Nawaank <mirtindayot@gmail.com>
2024-11-14 20:56:49 +0100

    presentation image css

--- pages/presentation.php
+++ pages/presentation.php
@@ -8,9 +8,11 @@
         <div class="description">
             <p>Bonjour, je suis Martin DAYOT. Passionné de photographie, je capture des moments uniques. Voici quelques-unes de mes œuvres.</p>
         </div>
-        <img src="../assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
-        <img src="../assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
-        <img src="../assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
+        <div class="illustrations">
+            <img src="../assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
+            <img src="../assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
+            <img src="../assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
+        </div>
     </div>
 </div>
 
