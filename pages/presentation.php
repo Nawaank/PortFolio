01c637cfe36a1dab5b96d91f15bf99326bf2bd22
Nawaank <mirtindayot@gmail.com>
2024-11-15 09:05:54 +0100

    css presentati

--- pages/presentation.php
+++ pages/presentation.php
@@ -4,16 +4,19 @@
 <div class="content">
     <h2>Présentation</h2>
     <div class="presentation">
-        <img src="../assets/images/photo_cv.jpg" alt="Martin DAYOT" class="photo-profil">
+        <div class="image-container">
+            <img src="../assets/images/photo_cv.jpg" alt="Martin DAYOT" class="photo-profil">
+            <div class="illustrations">
+                <img src="../assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
+                <img src="../assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
+                <img src="../assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
+            </div>
+        </div>
         <div class="description">
             <p>Bonjour, je suis Martin DAYOT. Passionné de photographie, je capture des moments uniques. Voici quelques-unes de mes œuvres.</p>
         </div>
-        <div class="illustrations">
-            <img src="../assets/images/bmw.jpg" alt="Illustration" class="photo-illustration">
-            <img src="../assets/images/porsche_color.png" alt="Illustration" class="photo-illustration">
-            <img src="../assets/images/mclaren.jpg" alt="Illustration" class="photo-illustration">
-        </div>
     </div>
 </div>
+
 
 <?php include('../footer.php'); ?>
