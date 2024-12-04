78b68c6340ad8a28f60f48bc81256158617ebfc8
Nawaank <mirtindayot@gmail.com>
2024-12-04 08:49:20 +0100

    ci: footer finish

--- header.php
+++ header.php
@@ -4,26 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
     <link rel="stylesheet" href="assets/css/header.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
     <title>Menu Adaptatif</title>
 </head>
 <body>
     <header>
         <nav>
-            <div class="title">Mon Site</div>
+            <div class="title">Martin DAYOT</div>
             <div class="burger">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
             <ul class="menu">
-                <li><a href="#home">Accueil</a></li>
-                <li><a href="#about">À propos</a></li>
-                <li><a href="#services">Services</a></li>
+                <li><a href="#home">Présentation</a></li>
+                <li><a href="#about">Mes photos</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     </header>
+    <body>
+        
+    </body>
+    <footer>
+        <p><span>&copy;</span> 2024 Martin DAYOT - Tous droits réservés</p>
+    </footer>
     <script src="assets/js/menuBg.js"></script>
 </body>
 </html>
